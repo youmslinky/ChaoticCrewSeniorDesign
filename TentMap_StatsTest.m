b3f9459ef446238a3642272b8ab590f51e5ee8c8
youmslinky <youmslinky1@gmail.com>
2020-10-23 16:51:18 -0500

    merge austin and my stat test

--- TentMap_StatsTest.m
+++ TentMap_StatsTest.m
@@ -1,9 +1,10 @@
 % Chaotic crew
 clear; close all;
-data_file_name = uigetfile("*.csv");
+[baseName, folder] = uigetfile("*.txt");
+data_file_name = fullfile(folder, baseName)
 %% 
 
-x = importdata(data_file_name);
+tentMapCircuitData = importdata(data_file_name);
 
 L = 1e5;
 m = 1.99999; %slope of tent map
@@ -19,6 +20,35 @@ m = 1.99999; %slope of tent map
 % end
 
 %plot raw values from tent map
+%% Variables
+T = 0.002756;
+f = 1/T;
+
+% time of all samples
+t = tentMapCircuitData.data(:,1);
+
+% values sampled at V(vc)
+vc = tentMapCircuitData.data(:,2);
+
+% samples of V(clk)
+clk = tentMapCircuitData.data(:,3);
+
+%midpoint of clock
+threshold = 2.5;
+
+%% Tent Map
+% nCrossing = 1;
+x = [];
+
+%count threshold x
+for i=1:uint64(length(clk)-1)
+    if clk(i)<threshold && clk(i+1)>threshold || clk(i)>threshold && clk(i+1)<threshold
+        x = [x, vc(i)];
+    else
+    end
+end
+
+
 figure;
 plot(x(1:1000));
 title('first 1000 tent map iteration values');
@@ -27,13 +57,13 @@ ylabel('value');
 
 %plot tent map by offsetting values
 figure;
-return_pairs = [x(1:end-1) x(2:end)];
+return_pairs = [x(1:end-1); x(2:end)];
 scatter(x(1:end-1), x(2:end));
-title('tent map')
-xlabel('x(n)')
-ylabel('x(n+1)')
+title('Tent Map')
+xlabel('X(n)')
+ylabel('X(n+1)')
 hold on
-% plot([0,1],[0,1]);
+scatter(x,x);
 hold off
 
 %plot histogram of tent map data
@@ -48,16 +78,16 @@ ylabel('counts')
 % fun = @(i) mean(x>=i)-0.5; partition = fzero(fun,1)
 
 %choose partition based on highest y value (should be top of graph
-[M,I] = max(return_pairs(:,2)); partition = x(I,1)
+[M,I] = max(return_pairs(2,:)); partition = x(I)
 s = (x>=partition);
 
 %calculate autocorrelation (cross correlation with itself) of bitstream
 [cc,lags] = xcorr(s-mean(s), s-mean(s), 'coeff');
 figure;
 plot(lags, abs(cc));
-title('autocorrelation')
+title('AutoCorrelation')
 
-%{
+
 k = 1;
 clear vnbits;
 for j = 2:2:length(s)
@@ -69,17 +99,17 @@ for j = 2:2:length(s)
         k=k+1;
     end
 end
-%}
 
-%gggggggggggggggggggggg
 
 %%% Built in MATLAB Stats Test
 
 bits_used = s;
+% bits_used = vnbits;
 
 %mean of the bitstream should be close to 0.5 aka, equal amounts of 0s and 1s
-xbar = mean(bits_used)
+fprintf('Mean of Bits: %f\n\n', mean(bits_used))
 
+%% Recurrence Plot
 figure;
 LN_bits = length(bits_used);
 sq_LN_bits = floor(sqrt(LN_bits));
@@ -88,38 +118,62 @@ recurrence_plotter = zeros(sq_LN_bits, sq_LN_bits);
 for j = 1:sq_LN_bits
     recurrence_plotter(1:sq_LN_bits,j) = bits_used2((j-1)*sq_LN_bits+1:j*sq_LN_bits);
 end
-image_matrix = 1-recurrence_plotter;
-imshow(imresize(image_matrix,4,'nearest'));
 
+a = 1-recurrence_plotter;
+c = imresize(a, 10, 'nearest');
+imshow(c)
+title('Recurrence Plot')
 
+%% bins
 clear bn;
 SL = 12;
 b = (1/2).^(1:SL);
 bn = zeros(1,length(bits_used)-SL);
 for i = 1:length(bits_used)-SL
-    bn(i) = b*bits_used(i:i+SL-1);
+    bn(i) = b*bits_used(i:i+SL-1)';
 end
 
+
+%% Histogram
 figure;
 hist(bn,100);
+title('Histogram of Circuit Output')
+xlabel('Measured values')
+ylabel('Count')
 
-
+%% PDF
+figure;
 db = 0.01;
 bins = 0:db:1;
 xhist = hist(bn,bins);
 xhist = xhist/sum(xhist)/db;
-figure;
 plot(bins, xhist);
-xlabel('x');
+xlabel('X');
 ylabel('PDF');
+title('Probablility Desity Function')
 
+%% FFT
+T = 0.002756;
+f = 1/T;
 
+% Center all points around 0
+
+figure;
+center_bn = bn - mean(bn);
+y = fft(center_bn);
+f1 = 0:f/length(y):f-f/length(y);
+plot(f1,y);
+xlabel('Frequency (Hz)')
+ylabel('Amplitude')
+title('Fourier Transform')
+
+%% Stats Tests
 [h1,p1] = runstest(bits_used);
 h1;     %% ZERO == PASS : ONE == FAIL
 if h1 == 0
-    fprintf('RunsTest: PASS\n')
+    fprintf('RunsTest: PASS\n\n')
 else
-    fprintf('RunsTest: FAIL\n')
+    fprintf('RunsTest: FAIL\n\n')
 end
 
 %chi2gof compares a distribution to a known distribution
@@ -128,8 +182,8 @@ pd = makedist('Uniform');
 [h2,p2] = chi2gof(bn, 'CDF', pd);
 h2;     %% ZERO == PASS : ONE == FAIL
 if h2 == 0
-    fprintf('Chi2 Test: PASS\n')
+    fprintf('Chi2 Test: PASS\n\n')
 else
-    fprintf('Chi2 Test: FAIL\n')
+    fprintf('Chi2 Test: FAIL\n\n')
 end
 
